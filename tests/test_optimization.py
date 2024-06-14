d8da66e8c410a608c24a2a31e2d2d69809b18358
4R1FG <167780601+4R1FG@users.noreply.github.com>
2024-06-14 16:31:34 +0200

    Update test_optimization.py

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -64,7 +64,7 @@ def test_optimization():
     
     """
     p = psso.LV_grid(network_data, active_profile, reactive_profile, ev_active_profile, meta_data)
-    p.N_1_calculation(18)
+    p.n_1_calculation(18)
     p.EV_penetration_level(0.8)
 
 
