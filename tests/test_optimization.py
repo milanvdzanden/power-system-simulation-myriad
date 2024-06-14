1b566ef24c25458b1cd02f3f5fb6ae6323bf384f
4R1FG <167780601+4R1FG@users.noreply.github.com>
2024-06-14 16:18:54 +0200

    switched calling of test functions

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -161,10 +161,9 @@ def test_errors():
     with pytest.raises(psso.EvProfilesDontMatchSymLoad) as excinfo:
         psso.LV_grid(network_data, active_profile, reactive_profile, test_number_EV, meta_data)
 
-test_errors()
-
 p = psso.LVGrid(network_data, active_profile, reactive_profile, ev_active_profile, meta_data)
 print(p.n_1_calculation(18))
     
 test_optimization()
+test_errors()
 
